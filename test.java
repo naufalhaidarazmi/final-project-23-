19f104c99b41d1f90912d45f405848a9a8ba34b4
naufalhaidarazmi <naufalhaidar946@gmail.com>
2025-09-24 14:04:06 +0700

    repair test

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public static void main(String[] args) {
     public static void main(String[] args) {
-        System.err.println("hi ini code pertama saya");
+        System.out.println("hi ini code pertama saya");
+        
     }
-}
+}                   
