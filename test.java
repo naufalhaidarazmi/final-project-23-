ded69a44a4a804612692eb63fc05ca2cfd43280c
naufalhaidarazmi <naufalhaidar946@gmail.com>
2025-09-24 14:21:31 +0700

    test merge conflic

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("ini baris 1");
-        System.out.println("ini baris dua dari user 1");
+        System.out.println("ini halo dari user 1");
     }
 }
