73bef266c774f08acb90c3db85eac8015bda7590
naufalhaidarazmi <naufalhaidar946@gmail.com>
2025-09-24 14:28:27 +0700

    commit after merge collide

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("ini baris 1");
-        System.out.println("halo bandung");
+        System.out.println("ini halo dari user 1 dan belum final ");
     }
 }
